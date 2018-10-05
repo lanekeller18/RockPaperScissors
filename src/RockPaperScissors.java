cd95a129965287d6c272c666267db37eaf20ed4d
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-10-05 07:59:57 -0400

    Added some basic code

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RockPaperScissors {
+
+    public static void main(String[]args){
+        Scanner keyboard;
+
+
+        String paper;
+        String rock;
+        String scissors;
+
+
+        keyboard = new Scanner(System.in);
+
+    }
+}
