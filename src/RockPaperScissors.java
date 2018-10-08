0881e166630e513a8107070d1a14fd75e5e8637c
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-10-08 08:12:01 -0400

    Added a loop and variable things

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -6,12 +6,38 @@ public class RockPaperScissors {
         Scanner keyboard;
 
 
-        String paper;
-        String rock;
-        String scissors;
+        int paper;
+        int rock;
+        int scissors;
+        String userPicked;
+        int computerNumber;
+        String again;
+        rock=1;
+        paper=2;
+        scissors=3;
 
 
         keyboard = new Scanner(System.in);
+        do {
+            System.out.println("Choose a number, 1-3 (Rock=1 Paper=2 Scissors=3.");
+            System.out.println("What would you like to choose first?");
+            userPicked = keyboard.nextLine();
+            computerNumber = (int)(Math.random() * 3)+1;
+            System.out.println(computerNumber);
+            again = "";
+
+            if
+
+
+
+
+
+            System.out.println("Would you like to play again?");
+            again = keyboard.next().toLowerCase();
+
+        }while (again.equals("yes"));
+        System.out.println("Oh well I didn't like you anyways.");
+
 
     }
 }
