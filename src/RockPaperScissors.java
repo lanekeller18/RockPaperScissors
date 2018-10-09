8048cab08751ac4cb73ee23e5222488096727822
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-10-09 07:41:05 -0400

    Added a lot of if else statements.

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -9,7 +9,7 @@ public class RockPaperScissors {
         int paper;
         int rock;
         int scissors;
-        String userPicked;
+        int userPicked;
         int computerNumber;
         String again;
         rock=1;
@@ -21,18 +21,40 @@ public class RockPaperScissors {
         do {
             System.out.println("Choose a number, 1-3 (Rock=1 Paper=2 Scissors=3.");
             System.out.println("What would you like to choose first?");
-            userPicked = keyboard.nextLine();
+            userPicked = keyboard.nextInt();
             computerNumber = (int)(Math.random() * 3)+1;
             System.out.println(computerNumber);
             again = "";
 
-            if
+
+            if(userPicked==computerNumber){
+                System.out.println("Aww its a tie.");
+            }
+            else if (userPicked==1 && computerNumber==2){
+                System.out.println("Haha! I win!");
+            }
+            else if (userPicked==1 && computerNumber==3){
+                System.out.println("Aww, you win this time.");
+
+            }
+            else if (userPicked==2 && computerNumber==1){
+                System.out.println("Gosh, you win.");
+            }
+            else if (userPicked==2 && computerNumber==3){
+                System.out.println("Haha kid, get better.");
+            }
+            else if (userPicked==3 && computerNumber==1){
+                System.out.println("Why are you so bad at this?");
+            }
+            else if (userPicked==3 && computerNumber==2){
+                System.out.println("Your so lucky my god.");
+            }
 
 
 
 
 
-            System.out.println("Would you like to play again?");
+            System.out.println("Would you like to play again? (yes or no)");
             again = keyboard.next().toLowerCase();
 
         }while (again.equals("yes"));
