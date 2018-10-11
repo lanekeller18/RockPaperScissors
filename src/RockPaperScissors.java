b625a58a541a571d7cd5fba863d0752561a28835
Lane Keller <lane.keller.2022@student.lhschools.org>
2018-10-11 07:51:18 -0400

    added the score system.

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -38,42 +38,55 @@ public class RockPaperScissors {
 
             if(userPicked==computerNumber){
                 System.out.println("Aww its a tie.");
+                System.out.println("No points, why would you get points for a tie?");
             }
             else if (userPicked==1 && computerNumber==2){
                 System.out.println("Haha! I win!");
+                computerScore +=1;
+                System.out.println("I get 1 point.");
 
             }
             else if (userPicked==1 && computerNumber==3){
                 System.out.println("Aww, you win this time.");
-
+                playerScore +=1;
+                System.out.println("You get 1 point.");
             }
             else if (userPicked==2 && computerNumber==1){
                 System.out.println("Gosh, you win.");
+                playerScore +=1;
+                System.out.println("You get 1 point.");
             }
             else if (userPicked==2 && computerNumber==3){
                 System.out.println("Haha kid, get better.");
+                computerScore +=1;
+                System.out.println("I get 1 point.");
             }
             else if (userPicked==3 && computerNumber==1){
                 System.out.println("Why are you so bad at this?");
+                computerScore +=1;
+                System.out.println("I get 1 point.");
             }
             else if (userPicked==3 && computerNumber==2){
                 System.out.println("Your so lucky my god.");
+                playerScore +=1;
+                System.out.println("You get 1 point.");
             }
 
 
 
 
-            System.out.println("Your score is "+ playerScore+ "while mine is "+ computerScore+ ".");
+            System.out.println("Your score is "+ playerScore+ " while mine is "+ computerScore+ ".");
             System.out.println("Would you like to play again? (yes or no)");
             again = keyboard.next().toLowerCase();
 
         }while (again.equals("yes"));
-        System.out.println("Your final score is " +playerScore+ " while, my score is "+ computerScore+".");
+        System.out.println("Your final score is " + playerScore+".");
+        System.out.println("While, my final score is "+ computerScore+".");
         if (playerScore==computerScore){
-        System.out.println("It is a tie between use over all!");
+        System.out.println("It is a tie between us over all!");
         }
         else if (playerScore<computerScore){
-            System.out.println("Haha, I won is the long run!");
+            System.out.println("Haha, I won in the long run!");
         }
         else if (playerScore>computerScore){
             System.out.println("The only way you beat me is because you cheated.");
